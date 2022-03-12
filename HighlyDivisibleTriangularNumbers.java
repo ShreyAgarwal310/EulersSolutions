80b3cc6c94cee5ff0c78c322c3e1e9a1d2af9757
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-12 17:36:29 -0600

    easy but interesting problem

---
+++ HighlyDivisibleTriangularNumbers.java
@@ -0,0 +1,37 @@
+public class HighlyDivisibleTriangularNumbers
+{
+    private static int NumberOfDivisors(int num)
+    {
+        int numDivisors = 0;
+        int sqrt = (int)Math.sqrt(num);
+
+        for(int i = 1; i <= sqrt; i++)
+        {
+            if(num % i == 0)
+            {
+                numDivisors += 2;
+            }
+        }
+
+        if(sqrt*sqrt == num)
+        {
+            numDivisors--;
+        }
+
+        return numDivisors;
+    }
+
+    public static void main(String[] args)
+    {
+        int number = 0;
+        int i = 1;
+
+        while(NumberOfDivisors(number) < 500)
+        {
+            number += i;
+            i++;
+        }
+
+        System.out.print(number);
+    }
+}
