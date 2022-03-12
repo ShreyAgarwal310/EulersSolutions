b73e8c7d237091e3e846d619030e6969594379bf
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-12 13:48:08 -0600

    did largest prime factor

---
+++ LargestPrimeFactor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LargestPrimeFactor
+{
+    public static void main(String[] args)
+    {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter i Value: ");
+            long n = input.nextLong();
+            long number = n;
+            long largestPrimeFactor = n;
+            long i = 2;
+            while (i <= n && n != 1) {
+                if (n % i == 0) {
+                    n = n / i;
+                    largestPrimeFactor = i;
+                }
+                else {
+                    i = i+1;
+                }
+            }
+            System.out.println("The largest prime factor of the number "+ number + " is "+ largestPrimeFactor);
+        }
+    }
+}
