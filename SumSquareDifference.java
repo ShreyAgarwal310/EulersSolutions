9634dac98e1373b76f815b4248e7b0d2db06dea2
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-12 14:13:45 -0600

    Create SumSquareDifference.java

---
+++ SumSquareDifference.java
@@ -0,0 +1,19 @@
+public class SumSquareDifference {
+    public static void main(String[] args)
+    {
+        int n = 100;
+        int sum = 0;
+        int sumOfSquares = 0;
+        int squareOfSum;
+
+        for(int i = 1; i <= n; i++)
+        {
+            sum += i;
+            sumOfSquares += i*i;
+        }
+
+        squareOfSum = sum*sum;
+
+        System.out.print("The difference between the sum of the squares and the square of the sums is " + (squareOfSum-sumOfSquares));
+    }
+}
