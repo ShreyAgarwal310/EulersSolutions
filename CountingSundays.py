03da22c9bd5fb6106e86f52758aca1d28e3f15bb
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-13 10:01:22 -0500

    Create CountingSundays.py

---
+++ CountingSundays.py
@@ -0,0 +1,10 @@
+from datetime import *
+
+sundays = 0
+
+for year in range(1901, 2001):
+    for month in range(1, 13):
+        if(date(year, month, 1).weekday() == 6):
+            sundays += 1
+
+print(sundays)
