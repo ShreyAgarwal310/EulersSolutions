dfb6c4680c2cf86aea996e26f5047d677596b604
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-14 18:09:17 -0500

    Create DigitFactorials.java

---
+++ DigitFactorials.java
@@ -0,0 +1,38 @@
+public class DigitFactorials
+{
+    public static int factorial(int n)
+    {
+        int fact = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args)
+    {
+        int sum = 0;
+
+        for(int i = 10; i <= 2540161; i++)
+        {
+            int sumOfFact = 0;
+            int number = i;
+
+            while(number > 0)
+            {
+                int d = number % 10;
+                number /= 10;
+                sumOfFact += factorial(d);
+            }
+
+            if(sumOfFact == i)
+            {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
