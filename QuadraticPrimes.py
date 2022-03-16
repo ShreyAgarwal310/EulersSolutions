afac4556caa77a0bb6ed0a4b5dc485436a2a4b3f
Shrey Agarwal <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-16 09:01:55 -0500

    Update QuadraticPrimes.py

--- QuadraticPrimes.py
+++ QuadraticPrimes.py
@@ -1,46 +1,61 @@
-import math
-
-def isPrime(n):
-    sqrt = math.sqrt(n)
-    for i in range(int(n), int(sqrt + 1)):
-        if(n % i == 0):
-            return False
-    return True
-
 def sieve(n):
     isPrime = [True] * n
     isPrime[0] = False
     isPrime[1] = False
-    isPrime[2] = True
-    
-    for i in range(3, int(n ** 0.5 + 1), 2):
-        index = i * 2
-        while(index < n):
-            isPrime[index] = False
-            index += i
-    
-    prime = [2]
 
-    for i in range(3, n, 2):
-        if(isPrime[i]):
+    for i in range(2, int(n ** 0.5 + 1)):
+        index = i * 2
+        while index < n:
+            isPrime[index] = False
+            index = index + i
+
+    prime = []
+
+    for i in range(n):
+        if isPrime[i] == True:
             prime.append(i)
-    
+
     return prime
 
-aMax = 0
-bMax = 0
-nMax = 0
+def isPrime(n):
+    for i in range(2, int(abs(n) ** 0.5) + 1):
+        if(n % i == 0):
+            return False
 
-primes = sieve(87400)
+    return True
 
-for a in range(-1000, 1001):
-    for b in range(-1000, 1001):
-        n = 0
-        while(isPrime(abs(n * n + a * n + b))):
-            n += 1
-        if(n > nMax):
-            aMax = a
-            bMax = b
-            nMax = n
+primes1000 = sieve(1000)
 
-print(n)
+primes = primes1000[:]
+
+largest = 0
+axb = 0
+
+for b in primes1000:
+    for a in primes1000:
+        i = 0
+        while True:
+            quadratic = i ** 2 + a * i + b
+            if(quadratic not in primes):
+                if(isPrime(quadratic)):
+                    primes.append(quadratic)
+                else:
+                    if(i - 1 > largest):
+                        largest = i - 1
+                        axb = a * b
+                    break
+            i += 1
+        i = 0
+        while True:
+            quadratic = i ** 2 - a * i + b
+            if(quadratic not in primes):
+                if(isPrime(quadratic) and quadratic > 0):
+                    primes.append(quadratic)
+                else:
+                    if(i - 1 > largest):
+                        largest = i - 1
+                        axb = -1 * a * b
+                    break
+            i += 1
+
+print(axb)
