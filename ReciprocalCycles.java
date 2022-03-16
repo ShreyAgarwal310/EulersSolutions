87afd58efca7d6477bdd87884f8030831722c489
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-16 08:52:30 -0500

    re did #27

---
+++ ReciprocalCycles.java
@@ -0,0 +1,35 @@
+public class ReciprocalCycles {
+    public static void main(String[] args)
+    {
+        int sequenceLength = 0;
+        int num = 0;
+ 
+        for(int i = 1000; i > 1; i--) 
+        {
+            if(sequenceLength >= i) 
+            {
+                break;
+            }
+        
+            int[] foundRemainders = new int[i];
+            int value = 1;
+            int position = 0;
+        
+            while(foundRemainders[value] == 0 && value != 0)
+            {
+                foundRemainders[value] = position;
+                value *= 10;
+                value %= i;
+                position++;
+            }
+        
+            if (position - foundRemainders[value] > sequenceLength) 
+            {
+                num = i;
+                sequenceLength = position - foundRemainders[value];
+            }
+        }
+
+        System.out.print(num);
+    }
+}
