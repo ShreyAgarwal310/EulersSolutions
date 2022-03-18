9d2805b27f35f69a527a597cc7ccfe42d50ee2fc
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-18 08:40:56 -0500

    Create CoinSums.py

---
+++ CoinSums.py
@@ -0,0 +1,11 @@
+counter = 0
+
+for a in range(3):
+    for b in range(int(1 + (200 - 100 * a) / 50)):
+        for c in range(int(1 + (200 - 100 * a - 50 * b) / 20)):
+            for d in range(int(1 + (200 - 100 * a - 50 * b - 20 * c) / 10)):
+                for e in range(int(1 + (200 - 100 * a - 50 * b - 20 * c - 10 * d) / 5)):
+                    for f in range(int(1 + (200 - 100 * a - 50 * b - 20 * c - 10 * d - 5 * e) / 2)):
+                        counter += 1
+
+print(counter + 1)
