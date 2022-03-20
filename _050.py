44f6fc7490502558f954d99ae1849d46824f59ad
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-20 14:24:11 -0500

    tried the usefulFunctions folder

--- _050.py
+++ _050.py
@@ -1,18 +1,4 @@
-def sieve(n):
-    isPrime = [True] * n
-    isPrime[0] = False
-    isPrime[1] = False
-    isPrime[2] = True
-    for i in range(3, int(n ** 0.5 + 1), 2):
-        index = i * 2
-        while(index < n):
-            isPrime[index] = False
-            index += i
-    prime = [2]
-    for i in range(3, n, 2):
-        if(isPrime[i]):
-            prime.append(i)
-    return prime
+from usefulFunctions.sieveOfEratosthenes import sieve
 
 primes = sieve(1000000)
 length = 0
